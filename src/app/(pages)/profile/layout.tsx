22547fc0fc463911f4705f0c93889ae0f6ca564d
Sadlize <sadlize@mail.ru>
2024-10-17 16:08:07 +0300

    chore: check sidebar

--- src/app/(pages)/profile/layout.tsx
+++ src/app/(pages)/profile/layout.tsx
@@ -1,17 +1,5 @@
-import '../../styles/normalize.css';
-import '../../styles/variables.scss';
-import '../../styles/app.scss';
-
-import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import { getMetadata } from 'shared/lib/metadata';
 import { Sidebar } from 'widgets/sidebar';
-
-export async function generateMetadata(): Promise<Metadata> {
-  // const globals = await globalsApi.get();
-  // return getMetadata(globals.data.meta);
-  return getMetadata();
-}
 
 export default async function ProfileLayout({ children }: { children: ReactNode }) {
   return (
--- src/widgets/sidebar/ui/sidebar-nav.tsx
+++ src/widgets/sidebar/ui/sidebar-nav.tsx
@@ -4,78 +4,55 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 import { useState } from 'react';
-import { DashboardPageIcon } from 'shared/assets/icons/dashboard-page-icon';
-import { LogoutIcon } from 'shared/assets/icons/logout-icon';
-import { NavSidebarIcon } from 'shared/assets/icons/nav-sidebar-icon';
-import { ProfilePageIcon } from 'shared/assets/icons/profile-page-icon';
-import { TransactionsPageIcon } from 'shared/assets/icons/transactions-page-icon';
+import { getPath } from 'shared/routing/paths';
+import { Button } from 'shared/ui/button';
+import { DashboardPageIcon } from 'shared/ui/icons/dashboard-page-icon';
+import { LogoutIcon } from 'shared/ui/icons/logout-icon';
+import { NavSidebarIcon } from 'shared/ui/icons/nav-sidebar-icon';
+import { ProfilePageIcon } from 'shared/ui/icons/profile-page-icon';
+import { TransactionsPageIcon } from 'shared/ui/icons/transactions-page-icon';
 
 import styles from './sidebar-nav.module.scss';
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleSidebar = () => setIsOpen(!isOpen);
+  const toggleSidebar = () => setIsOpen((prev) => !prev);
+
+  const signOutHandler = (e: React.MouseEvent | React.KeyboardEvent) => {
+    e.preventDefault();
+    if (isOpen) {
+      signOut({ callbackUrl: '/' });
+    }
+  };
 
   return (
-    <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
-      <div className={styles.toggleButton} role="presentation" onClick={toggleSidebar} onKeyDown={toggleSidebar}>
+    <aside className={clsx(styles.sidebar, isOpen && styles.open)}>
+      <Button aria-label="Menu" className={styles.navItem} onClick={toggleSidebar} onKeyDown={toggleSidebar}>
         <NavSidebarIcon />
-      </div>
+      </Button>
       <nav className={styles.navGroup}>
-        <div className={clsx(styles.navItem, isOpen && styles.clickable)}>
-          <Link
-            href="/profile/dashboard"
-            className={clsx(styles.link, !isOpen && styles.disabledLink)}
-            onClick={(e) => !isOpen && e.preventDefault()}
-          >
-            <DashboardPageIcon />
-            {isOpen && <span>Статистика</span>}
-          </Link>
-        </div>
-        <div className={clsx(styles.navItem, isOpen && styles.clickable)}>
-          <Link
-            href="/profile/transactions"
-            className={clsx(styles.link, !isOpen && styles.disabledLink)}
-            onClick={(e) => !isOpen && e.preventDefault()}
-          >
-            <TransactionsPageIcon />
-            {isOpen && <span>Транзакции</span>}
-          </Link>
-        </div>
-        <div className={clsx(styles.navItem, isOpen && styles.clickable)}>
-          <Link
-            href="/profile/settings"
-            className={clsx(styles.link, !isOpen && styles.disabledLink)}
-            onClick={(e) => !isOpen && e.preventDefault()}
-          >
-            <ProfilePageIcon />
-            {isOpen && <span>Настройки</span>}
-          </Link>
-        </div>
-      </nav>
-      <nav className={styles.navGroup}>
-        <div
-          role="presentation"
-          className={clsx(styles.logout, isOpen && styles.clickable)}
-          onClick={(e) => {
-            if (!isOpen) {
-              e.preventDefault();
-            } else {
-              signOut({ callbackUrl: '/' });
-            }
-          }}
-          onKeyDown={(e) => {
-            if (!isOpen) {
-              e.preventDefault();
-            } else {
-              signOut({ callbackUrl: '/' });
-            }
-          }}
+        <Link href={getPath('dashboard')} className={styles.navItem}>
+          <DashboardPageIcon />
+          {isOpen && <span>Статистика</span>}
+        </Link>
+        <Link href={getPath('transactions')} className={styles.navItem}>
+          <TransactionsPageIcon />
+          {isOpen && <span>Транзакции</span>}
+        </Link>
+
+        <Link href={getPath('settings')} className={styles.navItem}>
+          <ProfilePageIcon />
+          {isOpen && <span>Настройки</span>}
+        </Link>
+        <Button
+          className={clsx(styles.navItem, styles.logout, isOpen && styles.clickable)}
+          onClick={signOutHandler}
+          onKeyDown={signOutHandler}
         >
           <LogoutIcon />
-          {isOpen && <span>Выйти</span>}
-        </div>
+          {isOpen && 'Выйти'}
+        </Button>
       </nav>
     </aside>
   );
