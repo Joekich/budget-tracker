8ed3f4bafe03d263137312d1b7eace50729d4c96
Evgeniy Filippov <Shelupons@gmail.com>
2024-12-11 19:55:54 +0300

    style: homepage

--- src/views/home/ui/homepage.tsx
+++ src/views/home/ui/homepage.tsx
@@ -1,19 +1,72 @@
+'use client';
+
 import Link from 'next/link';
-import { getPath } from 'shared/routing/paths';
+import { useRouter } from 'next/navigation';
+import { FaFileExport, FaFilter, FaGlobe, FaTools } from 'react-icons/fa';
+import { Button } from 'shared/ui/button';
 
 import styles from './homepage.module.scss';
 
 export function HomePage() {
+  const router = useRouter();
   return (
     <main className={styles.homePage}>
-      <section className={styles.buttonGroup}>
-        <h1>Budget Tracker</h1>
-        <Link href={getPath('signin')} className={styles.button}>
-          Войти
-        </Link>
-        <Link href={getPath('signup')} className={styles.button}>
-          Зарегистрироваться
-        </Link>
+      <section className={styles.featuresSection}>
+        <div className={styles.headerBlock}>
+          <h2>Управляйте своими финансами легко вместе с Budget Tracker</h2>
+        </div>
+        <div className={styles.contentBlock}>
+          <div className={styles.featuresList}>
+            <ul>
+              <li>
+                <FaTools className={styles.icon} />
+                <div className={styles.featureText}>
+                  <strong>Удобный и понятный инструмент</strong>
+                  <p>для ведения личного бюджета</p>
+                </div>
+              </li>
+              <li>
+                <FaFilter className={styles.icon} />
+                <div className={styles.featureText}>
+                  <strong>Гибкая система фильтров</strong>
+                  <p>и простой интерфейс для работы с операциями</p>
+                </div>
+              </li>
+              <li>
+                <FaFileExport className={styles.icon} />
+                <div className={styles.featureText}>
+                  <strong>Экспорт транзакций</strong>
+                  <p>в формате .xlsx</p>
+                </div>
+              </li>
+              <li>
+                <FaGlobe className={styles.icon} />
+                <div className={styles.featureText}>
+                  <strong>Смена языка и валюты</strong>
+                  <p>для удобства пользователей из разных стран</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.registration}>
+            <h3>Зарегистрируйтесь и попробуйте весь доступный функционал</h3>
+            <Button
+              className={styles.button}
+              theme="primary"
+              onClick={() => {
+                router.push('signup');
+              }}
+            >
+              Зарегистрироваться
+            </Button>
+            <p className={styles.loginText}>
+              Уже зарегистрированы? <br />
+              <Link href="/signin" className={styles.link}>
+                Войдите в свой аккаунт
+              </Link>
+            </p>
+          </div>
+        </div>
       </section>
     </main>
   );
